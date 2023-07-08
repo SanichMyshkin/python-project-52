97b8b78fac4a3c360557513363c73698e872656f
Александр Мышкин <sir.myshkin@gmail.com>
2023-07-08 23:22:47 +0300

    fs

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / "static"
+
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
+
