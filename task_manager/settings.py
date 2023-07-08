fd35c6de5c60e7ac0a2debf602ad7b4cad0fbab0
Александр Мышкин <sir.myshkin@gmail.com>
2023-07-08 09:38:55 +0300

    add class of url

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task_manager',  # добавляем сюда название проекта 
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'task_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'tempates'],   # Сюда добавляем располжение директориии шаблонов 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from task_manager import views
+
+
 urlpatterns = [
+    path('', views.IndexView.as_view()),
     path('admin/', admin.site.urls),
 ]
