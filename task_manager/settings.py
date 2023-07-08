4161b27ba744dfb143f580db05438d4e6990d5d2
Александр Мышкин <sir.myshkin@gmail.com>
2023-07-08 22:36:27 +0300

    config for railway deployment

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -14,10 +14,12 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
