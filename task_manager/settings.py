c0c992f284b20d7feca1b13a859767084e5d76fe
Александр Мышкин <sir.myshkin@gmail.com>
2023-07-09 13:01:46 +0300

    Some fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['website',
-                 '127.0.0.1', ]
+ALLOWED_HOST = ['*']
 
 # Application definition
 
