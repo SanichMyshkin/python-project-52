879eff52937bf68e94b7a62e3ffbe4c7f862d7ac
Александр Мышкин <sir.myshkin@gmail.com>
2023-07-09 13:05:37 +0300

    Some fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -124,7 +124,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = BASE_DIR / "static"
+# STATIC_ROOT = BASE_DIR / "static"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 
