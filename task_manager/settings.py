83f43915b060718522305ea6815dabdd678fa17d
Александр Мышкин <sir.myshkin@gmail.com>
2023-07-10 17:18:26 +0300

    Some fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -65,10 +65,8 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
-    }
+    'default': dj_database_url.config(conn_max_age=600,
+                                      conn_health_checks=True, )
 }
 
 db_from_env = dj_database_url.config(conn_max_age=600)
