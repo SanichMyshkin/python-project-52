a2958c1a7c95e7175c5f76c959db82771f9e0f71
Александр Мышкин <sir.myshkin@gmail.com>
2023-07-10 12:17:20 +0300

    Some fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -2,11 +2,9 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-load_dotenv(os.path.join(BASE_DIR, ".env"))
-
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG')
 
