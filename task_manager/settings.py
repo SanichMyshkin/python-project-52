102deeb0dd30443998e7f70cf5aec69523d95f43
Александр Мышкин <sir.myshkin@gmail.com>
2023-07-10 17:03:07 +0300

    Some fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -62,12 +62,26 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
+'''
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600,
                                       conn_health_checks=True, )
 }
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
+    }
+}
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+print(DATABASES)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
