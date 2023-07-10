81d3dc097fd3e46ba6d3b3d0be3d0117f35d4a90
Александр Мышкин <sir.myshkin@gmail.com>
2023-07-10 12:42:59 +0300

    Some fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -2,10 +2,12 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
+
 DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = [
