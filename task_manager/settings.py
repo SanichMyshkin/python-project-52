927639e820a61eeadbb822b2f0b945fc2b5133d3
Александр Мышкин <sir.myshkin@gmail.com>
2023-07-24 15:59:23 +0300

    Some fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -65,13 +65,7 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# для локальной разработки
-'''DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}'''
+
 
 # Нужно для деплоя!
 DATABASE_URL = os.getenv("DATABASE_URL")
