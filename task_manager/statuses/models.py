1de16913b8cf1db129cc08e133e95537347b58a7
Александр Мышкин <sir.myshkin@gmail.com>
2023-07-30 17:08:58 +0300

    Some fix

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from task_manager.translations.trans import NameForField
+
+names = NameForField()
 
 
 class Status(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200, unique=True,
+                            verbose_name=names.name)
 
     def __str__(self):
         return self.name
